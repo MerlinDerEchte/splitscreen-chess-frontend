4685ca67040b705777c08f78379827d9190aad64
MerlinDerEchte <merlinraabe@yahoo.de>
2021-04-05 17:02:04 +0200

    remis implemented

--- src/Components/Shared/Game/GameOver/GameOver.js
+++ src/Components/Shared/Game/GameOver/GameOver.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import './GameOver.scss';
+import isKingAttacked from '../../../../Model/GameLogic/isKingAttacked';
 import Colors from '../../constants/Colors';
-function GameOver(props){
+function GameOver(props) {
 
-    
-    return(
+ 
+    return (
         <div className="game-over-overlay">
             <div className="game-over-container">
                 <span> Game Over </span>
-                <span>{props.Board.turn === Colors.WHITE ? 'Black' : 'White'} won</span>
+                <span>{isKingAttacked(props.Board)
+                    ?
+                    (props.Board.turn === Colors.WHITE 
+                    ? 
+                    'Black' 
+                    : 
+                    
+                    'White') + ' won'
+                :
+
+                'Remis'}</span>
             </div>
         </div>
     )
