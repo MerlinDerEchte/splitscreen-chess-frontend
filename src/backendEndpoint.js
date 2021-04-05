eb0efde002919732a30c1429f8b0dda29212b3a1
MerlinDerEchte <merlinraabe@yahoo.de>
2021-04-05 14:26:24 +0200

    url

--- src/backendEndpoint.js
+++ src/backendEndpoint.js
@@ -1,4 +1,4 @@
-const backendEndpoint = "http://127.0.0.1:4001"
+// const backendEndpoint = "http://127.0.0.1:4001"
 
-// const backendEndpoint = "https://splitscreen-chess.herokuapp.com";
+const backendEndpoint = "https://splitscreen-chess.herokuapp.com";
 export default backendEndpoint;
